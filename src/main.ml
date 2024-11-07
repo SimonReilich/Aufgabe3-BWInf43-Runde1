758566e745b69d4c0ee702f7ff65542a1ff86f32
Simon <simon.reilich@tum.de>
2024-11-07 21:52:21 +0100

    started implementing a interval datastructure

--- src/main.ml
+++ src/main.ml
@@ -1,32 +1,13 @@
-module BinaryTree = struct
-  type tree = Node of (tree * int * tree) | None
-
-  let rec max_depth tree = match tree with
-    | None -> 0
-    | Node (l, _, r) -> let ll = max_depth l in let lr = max_depth r in if ll < lr then 1 + lr else 1 + ll
-
-  let reduce_left tree = match tree with
-    | Node (Node (ll, vl, rl), v, r) -> Node (ll, vl, Node (rl, v, r))
-    | _ -> raise (Invalid_argument "Invalid tree")
-    
-  let reduce_right tree = match tree with
-    | Node (l, v, Node (lr, vr, rr)) -> Node (Node (l, v, lr), vr, rr)
-    | _ -> raise (Invalid_argument "Invalid tree")
-
-  let rec balance tree = match tree with
-    | None -> tree
-    | Node (l, v, r) -> let l = balance l in let r = balance r in 
-    if max_depth l - max_depth r > 1 then reduce_right tree
-    else if max_depth l - max_depth r < -1 then reduce_left tree
-    else Node (l, v, r)
-
-  let insert tree x =
-    let rec helper tree = match tree with
-    | None -> Node (None, x, None)
-    | Node (l, y, r) -> if x = y then tree else if x < y then Node (helper l, y, r) else Node (l, y, helper r)
-  in balance (helper tree)
-
-  let from_list l = List.fold_left (insert) None l
+module Interval = struct
+  type interval = Border of (int * int) | Cut of (interval * interval) | EmptyInterval
+  let new_interval lower upper = if lower <= upper then Border (lower, upper) else raise (Invalid_argument "lower bound has to be less or equal to upper bound")
+  let rec get_lower_bound = function Border (l, _) -> l | EmptyInterval -> raise (Invalid_argument "cant get lower bound of empty interval!") | Cut (i1, i2) -> let lb1 = get_lower_bound i1 in let lb2 = get_lower_bound i2 in if lb1 < lb2 then lb1 else lb2
+  let rec get_upper_bound = function Border (_, u) -> u | EmptyInterval -> raise (Invalid_argument "cant get lower bound of empty interval!") | Cut (i1, i2) -> let ub1 = get_upper_bound i1 in let ub2 = get_upper_bound i2 in if ub1 > ub2 then ub1 else ub2
+  let simplify interval = Border (get_lower_bound interval, get_upper_bound interval)
+  let rec is_in (v : float) (interval : interval) = match interval with Border (l, u) -> Float.of_int(l) < v && v < Float.of_int(u) | EmptyInterval -> false | Cut (i1, i2) -> is_in v i1 && is_in v i2
+  let rec is_in (v : int) (interval : interval) = match interval with Border (l, u) -> l < v && v < u | EmptyInterval -> false | Cut (i1, i2) -> is_in v i1 && is_in v i2
+  let overlap interval1 interval2 = is_in (get_lower_bound interval1) interval2 || is_in (get_upper_bound interval1) interval2
+  let cut interval1 interval2 = if overlap interval1 interval2 then simplify (Cut (interval1, interval2)) else EmptyInterval
 end
 
 let _ = print_string "Hello World\n"
